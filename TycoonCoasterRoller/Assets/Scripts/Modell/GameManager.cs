a8aef8787540e312fa56019ba3564b9e0990b3ee
Beszteri Balázs <78915513+beszteribalazs@users.noreply.github.com>
2021-03-17 19:38:46 +0100

    Basic GameManger Script added

---
+++ TycoonCoasterRoller/Assets/Scripts/Modell/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] public BuildingSystem buildingSystem;
+    static public GameManager instance;
+
+
+    private void Awake()
+    {
+        instance = this;
+    }
+    
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+
+    public void ChangeSelectedType(BuildingTypeSO buildingTypeSO)
+    {
+        buildingSystem.SetSelectedBuildingType(buildingTypeSO);
+    }
+}
