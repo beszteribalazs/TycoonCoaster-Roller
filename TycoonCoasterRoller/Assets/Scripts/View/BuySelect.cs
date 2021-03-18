8ef91deb19b7826f5e7f60623851d084d5ab8404
Beszteri Balázs <78915513+beszteribalazs@users.noreply.github.com>
2021-03-18 12:25:56 +0100

    Redesigned Models

---
+++ TycoonCoasterRoller/Assets/Scripts/View/BuySelect.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuySelect : MonoBehaviour
+{
+    public GameObject buyMenu;
+    public Button buyButton;
+
+public bool check;
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        check = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.B))
+        {
+            buyMenu.SetActive(check);
+            check = !check;
+        }
+    }
+
+    public void SetCheck()
+    {
+        check = true;
+    }
+
+    public void BuyButtonSet()
+    {
+        buyMenu.SetActive(check);
+        check = !check;
+    }
+
+}
