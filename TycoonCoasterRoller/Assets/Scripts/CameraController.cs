2786543ccbe6aa756acedb60787b7fd024ce460a
Beszteri Balázs <78915513+beszteribalazs@users.noreply.github.com>
2021-03-23 11:40:54 +0100

    New CameraBorder settings

--- TycoonCoasterRoller/Assets/Scripts/CameraController.cs
+++ TycoonCoasterRoller/Assets/Scripts/CameraController.cs
@@ -20,12 +20,21 @@ public class CameraController : MonoBehaviour
     private Vector3 newZoom;
     public float maxZoom; 
     public float minZoom;
+    public float limitMinX;
+    public float limitMaxX;
+    public float limitMinZ;
+    public float limitMaxZ;
 
     private Vector3 dragStartPosition;
     private Vector3 dragCurrentPosition;
 
     void Start()
     {
+        limitMinX = 0;
+        limitMaxX = GameManager.instance.Width * 3;
+        limitMinZ = 0;
+        limitMaxZ = GameManager.instance.Width * 3;
+        
         newPosition = transform.position;
         newRotation = transform.rotation;
         newZoom = cameraTransform.localPosition;
@@ -99,31 +108,49 @@ public class CameraController : MonoBehaviour
         {
             newPosition += (transform.forward * movementSpeed);
         }
+
         if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
         {
             newPosition += (transform.forward * -movementSpeed);
         }
+
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
             newPosition += (transform.right * movementSpeed);
         }
+
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
             newPosition += (transform.right * -movementSpeed);
         }
-        
+
         //rotation
         if (Input.GetKey(KeyCode.Q))
         {
-            newRotation *= Quaternion.Euler(Vector3.up*rotationAmount);
+            newRotation *= Quaternion.Euler(Vector3.up * rotationAmount);
         }
+
         if (Input.GetKey(KeyCode.E))
         {
-            newRotation *= Quaternion.Euler(Vector3.up*-rotationAmount);
+            newRotation *= Quaternion.Euler(Vector3.up * -rotationAmount);
         }
+
+
+
+            
+        transform.position = Vector3.Lerp(transform.position, newPosition, Time.deltaTime * movementTime);
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, limitMinX, limitMaxX), transform.position.y, Mathf.Clamp(transform.position.z, limitMinZ, limitMaxZ));
+        newPosition=new Vector3(Mathf.Clamp(newPosition.x, limitMinX, limitMaxX), transform.position.y, Mathf.Clamp(newPosition.z, limitMinZ, limitMaxZ));
         
-        transform.position = Vector3.Lerp(transform.position,newPosition,Time.deltaTime*movementTime);
-        transform.rotation = Quaternion.Lerp(transform.rotation,newRotation,Time.deltaTime*movementTime);
+        transform.rotation = Quaternion.Lerp(transform.rotation,newRotation,Time.deltaTime * movementTime);
+        
+
+        
+        
+        
     }
+    
+    
+    
     
 }
