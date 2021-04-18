a302ed165e7610067aac19bd97a6fa79e6cbec14
Beszteri Balázs <78915513+beszteribalazs@users.noreply.github.com>
2021-04-18 21:02:21 +0200

    ButtonChanges

---
+++ TycoonCoasterRoller/Assets/Scripts/View/ButtonSwitch.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonSwitch : MonoBehaviour
+{
+    [SerializeField] Button stop;
+    [SerializeField] Button resume;
+    [SerializeField] Button twoX;
+    [SerializeField] Button threeX;
+
+    public void DisableButton()
+    {
+        stop.interactable = false;
+        resume.interactable = false;
+        twoX.interactable = false;
+        threeX.interactable = false;
+    }
+
+    public void EnableButton()
+    {
+        stop.interactable = true;
+        resume.interactable = true;
+        twoX.interactable = true;
+        threeX.interactable = true;
+    }
+    
+}
