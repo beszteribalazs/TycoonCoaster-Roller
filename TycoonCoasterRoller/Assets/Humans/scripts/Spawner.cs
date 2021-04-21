ce37330db3a51c553203897b9b3620e9b881965b
Erdős Tamás <AG78KX@inf.elte.hu>
2021-04-21 13:21:21 +0200

    Update Spawner.cs

--- TycoonCoasterRoller/Assets/Humans/scripts/Spawner.cs
+++ TycoonCoasterRoller/Assets/Humans/scripts/Spawner.cs
@@ -19,7 +19,7 @@ public class Spawner : MonoBehaviour{
             int available = (int)GameManager.instance.TotalCapacity - (int)GameManager.instance.CurrentVisitors; 
             if (available > 0){
                 float chance = ((available * 2f) / GameManager.instance.TotalCapacity);
-                Debug.Log(chance);
+                //Debug.Log(chance);
                 if (Random.Range(0f, 1f) <= chance){
                     SpawnVisitor(BuildingSystem.instance.entryPoint.position + new Vector3(1, 0, 1) * (BuildingSystem.instance.CellSize / 2));    
                 }
