beaf5583f6a43b5ecbb03a180ef1f451c3365983
Erdős Tamás <AG78KX@inf.elte.hu>
2021-04-21 20:57:57 +0200

    Building type setter

--- TycoonCoasterRoller/Assets/Scripts/Model/Bulding System/Building Controllers/Building.cs
+++ TycoonCoasterRoller/Assets/Scripts/Model/Bulding System/Building Controllers/Building.cs
@@ -2,8 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class Building : MonoBehaviour
-{
+public abstract class Building : MonoBehaviour{
     protected BuildingTypeSO buildingType;
     Vector2Int gridOrigin;
     BuildingTypeSO.Direction buildingDirection;
@@ -12,28 +11,28 @@ public abstract class Building : MonoBehaviour
     public Transform visual;
 
     public Vector3 Position => position;
-    public abstract float SellPrice { get; }
-    public abstract float Upkeep { get; }
-    public abstract float Income { get; }
-    public abstract float BreakChance { get; }
+    public abstract float SellPrice{ get; }
+    public abstract float Upkeep{ get; }
+    public abstract float Income{ get; }
+    public abstract float BreakChance{ get; }
 
-    public BuildingTypeSO Type => buildingType;
+    public BuildingTypeSO Type{
+        get => buildingType;
+        set => buildingType = value;
+    }
 
     public string Name => buildingType.buildingName;
 
-    public List<Vector2Int> GetGridPositionList()
-    {
+    public List<Vector2Int> GetGridPositionList(){
         return buildingType.GetPositionList(gridOrigin, buildingDirection);
     }
 
-    public void Destroy()
-    {
+    public void Destroy(){
         Destroy(this.gameObject);
     }
 
     public static Building SpawnBuilding(Vector3 worldPosition, Vector2Int gridOrigin,
-        BuildingTypeSO.Direction buildingDirection, BuildingTypeSO buildingType, List<Vector2Int> posList)
-    {
+        BuildingTypeSO.Direction buildingDirection, BuildingTypeSO buildingType, List<Vector2Int> posList){
         Quaternion worldRotation = Quaternion.Euler(0, buildingType.GetRotationAngle(buildingDirection), 0);
         Transform spawnedBuildingTransform = Instantiate(buildingType.prefab, worldPosition, worldRotation);
         Building spawnedBuilding = spawnedBuildingTransform.GetComponent<Building>();
@@ -48,8 +47,7 @@ public abstract class Building : MonoBehaviour
         return spawnedBuilding;
     }
 
-    public override string ToString()
-    {
+    public override string ToString(){
         return buildingType.buildingName;
     }
 }
