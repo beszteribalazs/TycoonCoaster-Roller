c8679b40de8e5717254a394eb9821834cb757247
Erdős Tamás <AG78KX@inf.elte.hu>
2021-04-22 11:56:52 +0200

    Spaw chance tweak

--- TycoonCoasterRoller/Assets/Scripts/HumanScripts/Spawner.cs
+++ TycoonCoasterRoller/Assets/Scripts/HumanScripts/Spawner.cs
@@ -17,10 +17,10 @@ public class Spawner : MonoBehaviour{
     void Update(){
         if (TimeManager.instance.Tick - lastSpawnedTick >= spawnFrequency){
             int available = (int)GameManager.instance.TotalCapacity - (int)GameManager.instance.CurrentVisitors; 
-            if (available > -20){
+            if (available > 0){
                 float chance = ((available * 2f) / GameManager.instance.TotalCapacity);
                 //Debug.Log(chance);
-                if (Random.Range(0f, 1f) <= chance){
+                if (Random.Range(0f, 1f) - 0.3f <= chance){
                     SpawnVisitor(BuildingSystem.instance.entryPoint.position + new Vector3(1, 0, 1) * (BuildingSystem.instance.CellSize / 2));    
                 }
                 lastSpawnedTick = TimeManager.instance.Tick;
